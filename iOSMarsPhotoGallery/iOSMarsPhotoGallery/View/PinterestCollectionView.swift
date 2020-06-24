c70ad6303fcf50a94acb7e37cf3537eed21e8c77
Pedro Trujillo V <ptrujillo@crunchy.co>
2020-06-23 21:18:06 -0400

    App loads photos from nasa api and shows them in view, fixing layout for swift ui

---
+++ iOSMarsPhotoGallery/iOSMarsPhotoGallery/View/PinterestCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  PinterestCollectionView.swift
+//  iOSMarsPhotoGallery
+//
+//  Created by Pedro Trujillo on 6/23/20.
+//  Copyright © 2020 Pedro Trujillo V. All rights reserved.
+//
+
+import Foundation
